a5b8cd186db3ed643bf2d946656d2eabf5095a36
unknown <kinchoily@hotmail.com>
2021-11-30 14:44:50 +0000

    Greeting.js - Update add states and handlers

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,8 +1,31 @@
+import { useState } from "react";
+
 const Greeting = () => {
+
+  const [changeText, setChangeText] = useState(false);
+  const [count, setCount] = useState(0);
+
+  const addToCountHandler = () => {
+    setCount((prevState) => prevState + 1);
+  };
+
+  const clearCounterHandler = () => {
+    setCount(0);
+  };
+
+  const changeTextHandler = () => {
+    setChangeText(true);
+  }
+
   return (
     <div>
       <h2>Hello World</h2>
-      <p>Its good to see you</p>
+      {!changeText && <p>Its good to see you</p>}
+      {changeText && <p>Text Changed!</p>}
+      <button onClick={changeTextHandler}>Change Text</button>
+      <button onClick={addToCountHandler}>Add to count</button>
+      <button onClick={clearCounterHandler}>Clear count</button>
+      <p>{count}</p>
     </div>
   );
 };
